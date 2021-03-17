ab0e9e37f3a00f39f0e04c0b69e945138b9a9f2e
matifandy <matifandy@gmail.com>
2021-03-17 17:32:20 -0300

    contact form

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,9 +1,22 @@
 import styles from "../styles/contact.module.css";
 import Head from "next/head";
+import { useForm } from "react-hook-form";
 import { Toolbar } from "../components/toolbar";
 import { Footer } from "../components/footer";
 
+type Profile = {
+  name: string;
+  email: string;
+  msj: string;
+};
+
 export default function Home() {
+  const { register, handleSubmit, errors } = useForm<Profile>();
+
+  const onSubmit = handleSubmit((data) => {
+    console.log(JSON.stringify(data));
+  });
+
   return (
     <>
       <Head>
@@ -20,6 +33,41 @@ export default function Home() {
             from you for an interview and a possible job offer. Or you can just
             talk to me. Don't hesitate to contact me.😎
           </h3>
+          <div className={styles.form}>
+            <form onSubmit={onSubmit}>
+              <div>
+                <input
+                  ref={register({ required: true })}
+                  id="name"
+                  name="name"
+                  type="text"
+                  placeholder="Type your Name"
+                />
+                {errors.name && <div className="error">Enter your name</div>}
+              </div>
+              <div>
+                <input
+                  ref={register({ required: true })}
+                  id="email"
+                  name="email"
+                  type="email"
+                  placeholder="Type your Email"
+                />
+                {errors.email && <div className="error">Enter your Email</div>}
+              </div>
+              <div>
+                <input
+                  ref={register({ required: true })}
+                  id="msj"
+                  name="msj"
+                  type="text"
+                  placeholder="Type your Message"
+                />
+                {errors.msj && <div className="error">Enter your Message</div>}
+              </div>
+              <button type="submit">Save</button>
+            </form>
+          </div>
         </div>
 
         <Footer />
