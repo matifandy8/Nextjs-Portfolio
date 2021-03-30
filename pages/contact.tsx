218171a3d172072e177caa1745d63de0ac69bc3c
matifandy <matifandy@gmail.com>
2021-03-30 19:57:25 -0300

    contact form

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -2,8 +2,21 @@ import styles from "../styles/contact.module.css";
 import Head from "next/head";
 import { Toolbar } from "../components/toolbar";
 import { Footer } from "../components/footer";
+import { useForm } from "react-hook-form";
+
+type Profile = {
+  name: string;
+  email: string;
+  msj: string;
+};
 
 export default function Home() {
+  const { register, handleSubmit, errors } = useForm<Profile>();
+
+  const onSubmit = handleSubmit((data) => {
+    alert(JSON.stringify(data));
+  });
+
   return (
     <>
       <Head>
@@ -21,6 +34,41 @@ export default function Home() {
               hear from you for an interview and a possible job offer. Or you
               can just talk to me. Don't hesitate to contact me.😎
             </h3>
+          </div>
+          <div className={styles.form}>
+            <form onSubmit={onSubmit}>
+              <div>
+                <label htmlFor="name"> Name</label>
+                <input
+                  ref={register({ required: true })}
+                  id="name"
+                  name="name"
+                  type="text"
+                />
+                {errors.name && <div className="error">Enter your name</div>}
+              </div>
+              <div>
+                <label htmlFor="email">Email</label>
+                <input
+                  ref={register({ required: true })}
+                  id="email"
+                  name="email"
+                  type="text"
+                />
+                {errors.email && <div className="error">Enter email</div>}
+              </div>
+              <div>
+                <label htmlFor="msj">Message</label>
+                <input
+                  ref={register({ required: true })}
+                  id="msj"
+                  name="msj"
+                  type="text"
+                />
+                {errors.msj && <div className="error">Enter your Message</div>}
+              </div>
+              <button type="submit">Save</button>
+            </form>
           </div>
         </div>
 
