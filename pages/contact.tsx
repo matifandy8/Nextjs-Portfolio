c6f492b40416417824f1b54af4344f835ae645a8
matifandy <matifandy@gmail.com>
2021-04-08 15:17:38 -0300

    contact sendemail

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -2,31 +2,30 @@ import styles from "../styles/contact.module.css";
 import Head from "next/head";
 import { Toolbar } from "../components/toolbar";
 import { Footer } from "../components/footer";
-import { useForm } from "react-hook-form";
 import { motion } from "framer-motion";
-import Axios from "axios";
-
-type Profile = {
-  name: string;
-  email: string;
-  msj: string;
-};
+import emailjs from "emailjs-com";
 
 export default function Home() {
-  const { register, handleSubmit, errors } = useForm<Profile>();
+  function sendEmail(e) {
+    e.preventDefault();
 
-  const onSubmit = handleSubmit((data) => {
-    alert(JSON.stringify(data));
-    console.log(data.name);
-    Axios({
-      method: "POST",
-      data: {
-        username: data.name,
-        email: data.email,
-      },
-      url: "http://localhost:3040/send-email",
-    }).then((res) => alert(res.data));
-  });
+    emailjs
+      .sendForm(
+        "service_1d469uf",
+        "template_2ddhr9l",
+        e.target,
+        "user_TqnRNT4dbq7BW3API9HgJ"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    e.target.reset();
+  }
 
   return (
     <>
@@ -65,40 +64,15 @@ export default function Home() {
               </h3>
             </div>
             <div className={styles.form}>
-              <form onSubmit={onSubmit}>
-                <div>
-                  <label htmlFor="name"> Name</label>
-                  <input
-                    ref={register({ required: true })}
-                    id="name"
-                    name="name"
-                    type="text"
-                  />
-                  {errors.name && <div className="error">Enter your name</div>}
-                </div>
-                <div>
-                  <label htmlFor="email">Email</label>
-                  <input
-                    ref={register({ required: true })}
-                    id="email"
-                    name="email"
-                    type="text"
-                  />
-                  {errors.email && <div className="error">Enter email</div>}
-                </div>
-                <div>
-                  <label htmlFor="msj">Message</label>
-                  <input
-                    ref={register({ required: true })}
-                    id="msj"
-                    name="msj"
-                    type="text"
-                  />
-                  {errors.msj && (
-                    <div className="error">Enter your Message</div>
-                  )}
-                </div>
-                <button type="submit">Save</button>
+              <form className="contact-form" onSubmit={sendEmail}>
+                <input type="hidden" name="contact_number" />
+                <label>Name</label>
+                <input type="text" name="name" />
+                <label>Email</label>
+                <input type="email" name="email" />
+                <label>Message</label>
+                <textarea name="message" />
+                <input className="btn" type="submit" value="Send" />
               </form>
             </div>
           </div>
