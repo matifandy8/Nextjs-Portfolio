42552252fec9fba1740994a6c00478d90af7b3f4
matifandy <matifandy@gmail.com>
2021-04-29 13:03:14 -0300

    article css

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -1,47 +1,44 @@
-import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 import Head from "next/head";
 import { Toolbar } from "../../components/toolbar";
 import { Footer } from "../../components/footer";
 import { motion } from "framer-motion";
+import styles from "../../styles/article.module.css";
 
 let client = require("contentful").createClient({
-    space: process.env.NEXT_CONTENTFUL_SPACE_ID,
-    accessToken: process.env.NEXT_CONTENTFUL_ACCESS_TOKEN,
-})
+  space: process.env.NEXT_CONTENTFUL_SPACE_ID,
+  accessToken: process.env.NEXT_CONTENTFUL_ACCESS_TOKEN,
+});
 
 export async function getStaticPaths() {
-   let data = await client.getEntries({
-       content_type: "article",
-   });
+  let data = await client.getEntries({
+    content_type: "article",
+  });
 
-    return {
-        paths: data.items.map((item) => ({
-         params: {slug: item.fields.slug},
-       
-
-        })),
-        fallback: false
-    };    
-    
-
+  return {
+    paths: data.items.map((item) => ({
+      params: { slug: item.fields.slug },
+    })),
+    fallback: false,
+  };
 }
 
-export async function getStaticProps({params}) {
-    let data = await client.getEntries({
-        content_type: "article",
-        "fields.slug": params.slug,
-    });
-    return {
-        props: {
-            article: data.items[0],
-        }
-    };
+export async function getStaticProps({ params }) {
+  let data = await client.getEntries({
+    content_type: "article",
+    "fields.slug": params.slug,
+  });
+  return {
+    props: {
+      article: data.items[0],
+    },
+  };
 }
 
-export default function Article({article}) {
-    console.log(article)
-    return( 
-        <>
+export default function Article({ article }) {
+  console.log(article);
+  return (
+    <>
       <Head>
         <title>Blog - Matias Fandiño</title>
         <meta name="description" />
@@ -67,13 +64,13 @@ export default function Article({article}) {
             },
           }}
         >
-    <div>
-       <h1>{article.fields.title}</h1>
-       <div>{documentToReactComponents(article.fields.content)}</div>
-    </div> 
-    </motion.div>
+          <div className={styles.article}>
+            <h1>{article.fields.title}</h1>
+            <div>{documentToReactComponents(article.fields.content)}</div>
+          </div>
+        </motion.div>
         <Footer />
       </div>
     </>
-    )
+  );
 }
