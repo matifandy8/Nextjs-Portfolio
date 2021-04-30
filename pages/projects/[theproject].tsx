e26be5e085ddd3c6aba724c783f246703300cf0a
matifandy <matifandy@gmail.com>
2021-04-30 19:27:56 -0300

    proyect

--- pages/projects/[theproject].tsx
+++ pages/projects/[theproject].tsx
@@ -1,4 +1,7 @@
+import { motion } from "framer-motion";
 import { useRouter } from "next/router";
+import { Footer } from "../../components/footer";
+import { Toolbar } from "../../components/toolbar";
 
 export default function theproject() {
   const router = useRouter();
@@ -6,8 +9,30 @@ export default function theproject() {
 
   return (
     <div>
-      <h2>proyect details</h2>
-      <p>proyect id: {router.query.theproject}</p>
+      <Toolbar />
+      <motion.div
+        initial="hidden"
+        animate="visible"
+        variants={{
+          hidden: {
+            scale: 0.8,
+            opacity: 0,
+          },
+          visible: {
+            scale: 1,
+            opacity: 1,
+            transition: {
+              duration: 0.5,
+              delay: 0.4,
+            },
+          },
+        }}
+      >
+        <h2>proyect details</h2>
+        <p>proyect id: {router.query.theproject}</p>
+      </motion.div>
+
+      <Footer />
     </div>
   );
 }
